1ffbf855e13269bcebe4c0f4a5a0f9e415afb0b5
Vedansh Wani <me.vedanshwani@gmail.com>
2021-08-19 14:59:40 +0530

    setup

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.send("hey");
+});
+
+const uri = process.env.ATLAS_URI;
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => app.listen(port))
+  .then(console.log("SERVER IS RUNNING"))
+  .catch((err) => console.log(err));
